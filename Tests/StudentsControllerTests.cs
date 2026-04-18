0b4dfae854d47fc635d3a23668b2322ee0aee6f7
wvasquezn-maker <wvasquezn@miumg.edu.gt>
2026-04-18 04:32:48 -0600

    Corrige asserts de prueba unitaria

--- Tests/StudentsControllerTests.cs
+++ Tests/StudentsControllerTests.cs
@@ -66,7 +66,7 @@ public class StudentsControllerTests
         var created = Assert.IsType<CreatedAtActionResult>(result.Result);
         var student = Assert.IsType<StudentResponseDto>(created.Value);
         Assert.Equal("William", student.FirstName);
-        Assert.Equal("Estudiante", student.LastName);
+        Assert.Equal("Vasquez", student.LastName);
         Assert.True(student.Id > 0);
     }
 
